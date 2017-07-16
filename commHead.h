798fdf31b5c71dc1c2d851006870fa757fb71353
shanwen zhu <zhushanwen321@hotmail.com>
2017-07-17 00:13:27 +0800

    letters combination of phone numbers

---
+++ commHead.h
@@ -0,0 +1,16 @@
+// contain common head file of c++
+
+#include <iostream>
+#include <cstdio>
+#include <fstream>
+#include <algorithm>
+#include <cmath>
+#include <deque>
+#include <vector>
+#include <queue>
+#include <string>
+#include <cstring>
+#include <map>
+#include <stack>
+#include <set>
+
